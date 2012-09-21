c8de1237c540c851dee5fe19c25d07ef6176c6b2
Emil Kampp <emil@kampp.me>
2012-09-21 06:50:26 +0200

    Allow rails 4 beta

--- lib/compass-rails/patches.rb
+++ lib/compass-rails/patches.rb
@@ -1,5 +1,5 @@
-if CompassRails.rails31? || CompassRails.rails32?
-  unless CompassRails.rails_loaded? 
+if CompassRails.rails31? || CompassRails.rails32? || CompassRails.rails4?
+  unless CompassRails.rails_loaded?
     CompassRails.load_rails
   end
   require 'compass-rails/patches/importer'
